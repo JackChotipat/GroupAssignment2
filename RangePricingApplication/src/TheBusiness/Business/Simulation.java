964a413f886c0265522e131548fde703d0dbae59
Yudan Zhou <zhou.yud@northeastern.edu>
2025-11-10 23:59:29 -0500

    Simulation created

---
+++ RangePricingApplication/src/TheBusiness/Business/Simulation.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TheBusiness.Business;
+
+/**
+ *
+ * @author 123
+ */
+public class Simulation {
+    
+}
