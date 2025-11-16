87d7053875ec063aa63761a06cdacfac97faf2ff
JackChotipat <metreethumaporn.c@northeastern.edu>
2025-11-16 00:14:13 -0500

    Comparison Class

--- RangePricingApplication/src/TheBusiness/Business/ProductPerformanceReport.java
+++ RangePricingApplication/src/TheBusiness/Business/ProductPerformanceReport.java
@@ -45,4 +45,53 @@ public class ProductPerformanceReport {
         }
     }
     
+    /**
+     * Combined before/after data for reporting
+     */
+    public static class ProductComparisonData {
+        public String supplierName;
+        public String productName;
+        
+        public int targetPriceBefore;
+        public int targetPriceAfter;
+        
+        public int salesAboveBefore;
+        public int salesBelowBefore;
+        public int salesAboveAfter;
+        public int salesBelowAfter;
+        
+        public int revenueBefore;
+        public int revenueAfter;
+        
+        public int profitBefore;
+        public int profitAfter;
+        
+        public int getTargetPriceChange() {
+            return targetPriceAfter - targetPriceBefore;
+        }
+        
+        public double getTargetPriceChangePercent() {
+            if (targetPriceBefore == 0) return 0;
+            return ((double)(targetPriceAfter - targetPriceBefore) / targetPriceBefore) * 100;
+        }
+        
+        public int getRevenueChange() {
+            return revenueAfter - revenueBefore;
+        }
+        
+        public double getRevenueChangePercent() {
+            if (revenueBefore == 0) return 0;
+            return ((double)(revenueAfter - revenueBefore) / revenueBefore) * 100;
+        }
+        
+        public int getProfitChange() {
+            return profitAfter - profitBefore;
+        }
+        
+        public double getProfitChangePercent() {
+            if (profitBefore == 0) return 0;
+            return ((double)(profitAfter - profitBefore) / profitBefore) * 100;
+        }
+    }
+    
 }
