febac77b7c1be70a20bd8f58c9d33030ef2833d0
edden-nu <chiu.yenc@northeastern.edu>
2025-11-16 21:40:59 -0500

    Update product detail

--- RangePricingApplication/src/UserInterface/ProductManagement/ManageProductPerformanceDetail.java
+++ RangePricingApplication/src/UserInterface/ProductManagement/ManageProductPerformanceDetail.java
@@ -33,7 +33,6 @@ public class ManageProductPerformanceDetail extends javax.swing.JPanel {
         this.business = business;
         this.selectedproduct = product;
         this.CardSequencePanel = jp;
-
         initComponents();
         populateProductDropdown();
         displayProductPerformance();
@@ -200,7 +199,7 @@ public class ManageProductPerformanceDetail extends javax.swing.JPanel {
         return;
     }
 
-    int newTarget = (int)(oldTarget * 1.1); // 漲 10%
+    int newTarget = (int)(oldTarget * 1.1);
     selectedproduct.updateProduct(
         selectedproduct.getFloorPrice(),
         selectedproduct.getCeilingPrice(),
